95de233763d258ded61770638d55d2a6ac659486
aysee <ayseekoc481@gmail.com>
2025-10-24 23:34:55 +0300

    first commit

---
+++ customer-service/src/test/java/com/aysekoc/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aysekoc.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
