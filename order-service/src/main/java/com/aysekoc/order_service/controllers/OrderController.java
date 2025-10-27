49f0c3746f580f625a6def11c597f245a595efa9
aysee <ayseekoc481@gmail.com>
2025-10-28 00:29:10 +0300

    .

--- order-service/src/main/java/com/aysekoc/order_service/controllers/OrderController.java
+++ order-service/src/main/java/com/aysekoc/order_service/controllers/OrderController.java
@@ -1,7 +1,10 @@
 package com.aysekoc.order_service.controllers;
 
 import com.aysekoc.order_service.client.ProductClient;
+import com.aysekoc.order_service.entity.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class OrderController {
     private final ProductClient productClient;
+    private final StreamBridge streamBridge;
 
     @GetMapping
     public String get() {
-        String response = productClient.get();
-        System.out.println(response);
+        //sync iletişim
+        //String response = productClient.get();
+        //System.out.println(response);
+
+        //kafkaya mesaj gönder
+
+        // TODO: Kafkadan common bir classı gönder.
+        Order order = new Order();
+        order.setId("abc123");
+        //
+
+
+        streamBridge.send("orderCreatedFunction-out-0","Mesaj123");
         return "Order Service";
     }
 }
---
+++ order-service/src/main/java/com/aysekoc/order_service/entity/Order.java
@@ -0,0 +1,10 @@
+package com.aysekoc.order_service.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Order {
+    private String id;
+}
