ad8c899859cc1e07c5e758d833424894f643046e
aysee <ayseekoc481@gmail.com>
2025-11-01 23:25:57 +0300

    .

--- order-service/src/main/java/com/aysekoc/order_service/entity/Order.java
+++ order-service/src/main/java/com/aysekoc/order_service/entity/Order.java
@@ -1,10 +1,25 @@
 package com.aysekoc.order_service.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "orders")
 public class Order {
-    private String id;
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
 }
