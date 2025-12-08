6ec9166838b9546258832f19512cf2bd4eda9f9e
aysekocc <ayseekoc481@gmail.com>
2025-12-08 13:35:05 +0300

    commit

--- cart-service/src/main/java/com/aysekoc/cart_service/controller/CartController.java
+++ cart-service/src/main/java/com/aysekoc/cart_service/controller/CartController.java
@@ -2,17 +2,20 @@ package com.aysekoc.cart_service.controller;
 
 import com.aysekoc.cart_service.entity.Cart;
 import com.aysekoc.cart_service.repository.CartRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/v1/cart")
-@AllArgsConstructor
 public class CartController {
 
     private final CartRepository cartRepository;
+
+    public CartController(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
 
     @GetMapping
     public Cart get(@RequestParam String name) {
